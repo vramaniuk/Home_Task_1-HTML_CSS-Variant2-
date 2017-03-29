539eff7cbd154cde2837eeff2fe5171a556bebe1
vramaniuk <astyanax@mail.ru>
2017-03-29 21:26:14 +0300

    Home Task 2(Working with DOM and AJAX) DONE (ver1.0)

---
+++ DOM_AJAX.js
@@ -0,0 +1,41 @@
+function fetchNews(source) {
+
+    const url = new URL('https://newsapi.org/v1/articles');
+
+    url.searchParams.append('source', source);
+    url.searchParams.append('sortBy', 'top');
+    url.searchParams.append('apiKey', '67df228ef66d4d799240542da2a606ce');
+
+    return fetch(url)
+        .then(response => response.json())
+        .then((result) => {
+            putMyArticle(result);
+        })
+        .catch(console.error);
+}
+
+
+function putMyArticle(result) {
+    const box = document.getElementById("box");
+    for (let i = 0; i < 10; i++) {
+        let goURL = result.articles[i].url;
+        let goImageURL = result.articles[i].urlToImage;
+        let date = new Date(result.articles[i].publishedAt).toUTCString();
+        let author = result.articles[i].author;
+        let title = result.articles[i].title;
+        let myArticle = document.createElement("article");
+        myArticle.innerHTML = `<a href="${goURL}"> <img src="${goImageURL}" alt="loading..."></a>
+<div style="color: grey">${date}</div>
+<div>${title}</div>
+<div style="color: grey"><span>Author(s):</span> ${author}</div>
+<a href=${goURL}>To see more</a>`;
+        box.appendChild(myArticle);
+    }
+}
+
+fetchNews("the-guardian-au");
+
+
+
+
+
