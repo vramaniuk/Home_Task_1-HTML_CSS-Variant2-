6bd407555bc1e76e53ad19a342bf06d5acd6c866
vramaniuk <astyanax@mail.ru>
2017-06-15 15:34:52 +0300

    Home Task 2(Working with DOM and AJAX) DONE (ver 1.4.7) window.onloadd changed to DOMContentLoaded

--- DOM_AJAX.js
+++ DOM_AJAX.js
@@ -1,60 +1,61 @@
-window.onload =
-    (function fetchNews(source) {
-        function get(url) {
-            return fetch(url);
-        }
+document.addEventListener("DOMContentLoaded", ready);
+   function ready() {
 
-        function setURL(source) {
-            const url = new URL('https://newsapi.org/v1/articles');
-            url.searchParams.append('source', source);
-            url.searchParams.append('sortBy', 'top');
-            url.searchParams.append('apiKey', '67df228ef66d4d799240542da2a606ce');
-            return url;
-        }
+       (function fetchNews(source) {
 
-        function sendRequest() {
-            return new Promise(function (resolve, reject) {
-                get(setURL(source))
-                    .then(response => {
-                        if (!response.ok)
-                            throw new Error(`${response.status} ( ${response.statusText} )`);
-                        resolve(response.json())
-                    })
-                    .catch(reject => alert(`Запрос к серверу не удался.Произошла ошибка: ${reject.message}`));
-            });
-        }
+           function setURL(source) {
+               const url = new URL('https://newsapi.org/v1/articles');
+               url.searchParams.append('source', source);
+               url.searchParams.append('sortBy', 'top');
+               url.searchParams.append('apiKey', '67df228ef66d4d799240542da2a606ce');
+               return url;
+           }
 
-        sendRequest().then(result => {
-            putMyArticle(result)
-        });
-        function Article(result, i) {
-            this.goURL = result.articles[i].url;
-            this.goImageURL = result.articles[i].urlToImage;
-            this.date = new Date(result.articles[i].publishedAt).toUTCString();
-            this.author = result.articles[i].author;
-            this.title = result.articles[i].title;
-            this.createOneArticle = function () {
-                let myArticle = document.createElement("article");
-                myArticle.innerHTML = `
+           function sendRequest() {
+               return new Promise(function (resolve, reject) {
+                   fetch(setURL(source))
+                       .then(response => {
+                           if (!response.ok)
+                               throw new Error(`${response.status} ( ${response.statusText} )`);
+                           resolve(response.json())
+                       })
+                       .catch(reject => alert(`Запрос к серверу не удался.Произошла ошибка: ${reject.message}`));
+               });
+           }
+
+           sendRequest().then(result => {
+               putMyArticle(result)
+           });
+           function Article(result, i) {
+               this.goURL = result.articles[i].url;
+               this.goImageURL = result.articles[i].urlToImage;
+               this.date = new Date(result.articles[i].publishedAt).toUTCString();
+               this.author = result.articles[i].author;
+               this.title = result.articles[i].title;
+               this.createOneArticle = function () {
+                   let myArticle = document.createElement("article");
+                   myArticle.innerHTML = `
 <a href="${this.goURL}"> <img src="${this.goImageURL}" alt="loading..."></a>
 <div style="color: grey">${this.date}</div>
 <div>${this.title}</div>
 <div style="color: grey"><span>Author(s):</span> ${this.author}</div>
 <a href=${this.goURL}>To see more</a>`;
-                return myArticle;
-            }
-        }
+                   return myArticle;
+               }
+           }
 
-        function putMyArticle(result) {
-            const box = document.getElementById("box");
-            let oneFullSide = document.createDocumentFragment();
-            for (let i = 0; i < result.articles.length; i++) {
-                let instanceOfArticle = new Article(result, i);
-                oneFullSide.appendChild(instanceOfArticle.createOneArticle());
-            }
-            box.appendChild(oneFullSide);
-        }
-    })("the-guardian-au");
+           function putMyArticle(result) {
+               const box = document.getElementById("box");
+               let oneFullSide = document.createDocumentFragment();
+               for (let i = 0; i < result.articles.length; i++) {
+                   let instanceOfArticle = new Article(result, i);
+                   oneFullSide.appendChild(instanceOfArticle.createOneArticle());
+               }
+               box.appendChild(oneFullSide);
+           }
+       })("the-guardian-au");
+
+   }
 
 
 
